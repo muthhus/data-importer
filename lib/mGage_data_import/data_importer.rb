50f980b4a521eb436aee06f2ea6af0cc0f0d6897
Shan <shan@telekomatrix.com>
2015-09-28 17:09:59 +0100

    dynamically pass the whitespace value.

--- lib/mGage_data_import/data_importer.rb
+++ lib/mGage_data_import/data_importer.rb
@@ -1,10 +1,10 @@
 module MGageDataImport
   class DataImporter
     
-    def send_vote locFileName
+    def send_vote locFileName, lSpace
       File.open(locFileName).readlines.each do |line|
-        arr_campaign = line.split(nil, 9)
-        if arr_campaign.size == 9
+        arr_campaign = line.split(nil, lSpace)
+        if arr_campaign.size == lSpace.to_ni
           result_type, epoch_time, campaign, validity, choice = arr_campaign[0], arr_campaign[1], arr_campaign[2].split(':')[1], arr_campaign[3].split(':')[1], arr_campaign[4].split(':')[1]
           if /[A-Za-z]/.match(result_type) && /[0-9]/.match(epoch_time) && /[a-zA-Z0-9_]/.match(campaign) && /[A-Z]/i.match(validity) && /[a-z]/i.match(choice)
             unless MGageDataImport::Campaign.table_exists?
@@ -17,6 +17,8 @@ module MGageDataImport
               @campaign.save!
             end
           end
+        else
+          puts "Check the property files format"
         end
           
       end
